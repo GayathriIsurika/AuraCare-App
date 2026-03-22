8f3b2a860b18eb731b347c9a53eed337a8dfec06
pramodlakshan125 <pramodlakshan125@gmail.com>
2026-03-22 09:08:20 +0530

    Add app colors constants

---
+++ lib/constant/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color background = Color(0xFFEAF8FB); // white blue bg
+const Color cardColor = Color(0xFFFFFFFF); // white
+
+const Color textDark = Colors.black; // dark text
+const Color textGrey = Colors.grey; // grey text
+const Color textLight = Colors.white; // white text
+
+const Color buttonStart = Color(0xFF72C6D5); // button start color
+const Color buttonEnd = Color(0xFF5DADE2); // button end color
+const Color buttonText = Color(0xFFFFFFFF); // white text for buttons
